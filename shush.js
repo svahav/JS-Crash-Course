282a93c8881a6cf24ede7e276c2e7f0304282fc4
Shira Shatz <vahav.shira1997@gmail.com>
2023-08-09 10:23:00 +0300

    salesforce

---
+++ shush.js
@@ -0,0 +1,25 @@
+// function calculateArea (width,height){
+//     const area = width * height;
+// }
+// console.log(calculateArea(2,5));
+function dayOfWeek(dayNumber ){
+    switch(dayNumber){
+        case 1:
+            console.log("Sunday");
+        case 2:
+            console.log("Monday"); 
+        case 3:
+            console.log("Tuesday"); 
+        case 4:
+            console.log("Wendesday");
+        case 5:
+            console.log("Thursday"); 
+        case 6:
+            console.log("Friday");  
+        case 7:
+            console.log("Suterday");     
+    }
+}
+
+const dayNumber1=3;
+dayOfWeek(dayNumber1);
